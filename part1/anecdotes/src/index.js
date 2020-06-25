ea4ae0c7449ef719308818d65a0e5e86b9cc27a3
awoken-coder <jfafandrade@gmail.com>
2020-06-25 18:13:55 +0100

    Finish last exercise of part1

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0]);
+  const [higherVote, setHigherVote] = useState(0);
+
+  const handleclick = () => {
+    const copy = [0,0,0,0,0,0];
+    copy[selected] += 1;
+    setVotes(votes.map((a,i)=>a+ copy[i]));
+    console.log(votes);
+    if (votes[selected]>=votes[higherVote]){setHigherVote(selected)}
+
+  }
+  const newAnecdote = () => {
+setSelected(Math.round(Math.random()*5));
+  }
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={newAnecdote}>next anecdote</button>
+      <button onClick={handleclick}>vote</button>
+     
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[higherVote]}</p>
+      <p>has {votes[higherVote]} votes</p>
+    </div>
+  )
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
