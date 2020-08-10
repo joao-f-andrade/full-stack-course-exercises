623b0a0daba1920c2164309f0020dcd91c74e7fa
awoken-coder <jfafandrade@gmail.com>
2020-08-10 16:34:13 +0100

    Added module with routes of the app and changed index accordingly

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const Blog = require('./models/blog')
-
+const blogsRouter = require('./controllers/blogs')
 
 
 mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -14,23 +14,8 @@ mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: t
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
