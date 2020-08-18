04a667fe78ae7f669de5fba061c43ab04f241076
awoken-coder <jfafandrade@gmail.com>
2020-08-18 14:31:36 +0100

    aglomerated tests under describe sections

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -16,77 +16,80 @@ beforeEach(async () => {
   }
 })
 
-test('blogs are returned as json', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-  expect(response.body).toHaveLength((await helper.blogsInDb()).length)
+describe('getting existing notes', () => {
+  test('blogs are returned as json', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    expect(response.body).toHaveLength((await helper.blogsInDb()).length)
+  })
 
+  test('blogs have an id', async () => {
+    const response = await api
+      .get('/api/blogs')
+    for (let blog of response.body) {
+      expect(blog.id).toBeDefined()
+      expect(blog._id).toBeUndefined()
+    }
+  })
 })
 
-test('blogs have an id', async () => {
-  const response = await api
-    .get('/api/blogs')
-  for (let blog of response.body) {
-    expect(blog.id).toBeDefined()
-    expect(blog._id).toBeUndefined()
-  }
-})
+describe('posting blogs', () => {
+  test('post blogs works', async () => {
+    const newBLog = {
+      'title': 'reddit',
+      'author': 'redditors',
+      'url': 'https://reddit.com',
+      'likes': 3
+    }
+    const oldBLogs = await helper.blogsInDb()
+    await api
+      .post('/api/blogs')
+      .send(newBLog)
+    const newBlogs = await helper.blogsInDb()
+    const lastBlog = newBlogs[newBlogs.length - 1]
+    delete lastBlog.id
+    expect(newBlogs)
+      .toHaveLength(oldBLogs.length + 1)
+    expect(lastBlog).toEqual(newBLog)
 
-test('post blogs works', async () => {
-  const newBLog = {
-    'title': 'reddit',
-    'author': 'redditors',
-    'url': 'https://reddit.com',
-    'likes': 3
-  }
-  const oldBLogs = await helper.blogsInDb()
-  await api
-    .post('/api/blogs')
-    .send(newBLog)
-  const newBlogs = await helper.blogsInDb()
-  const lastBlog = newBlogs[newBlogs.length - 1]
-  delete lastBlog.id
-  expect(newBlogs)
-    .toHaveLength(oldBLogs.length + 1)
-  expect(lastBlog).toEqual(newBLog)
+  })
 
-})
+  test('blogs without likes property will have it set to 0', async () => {
+    const newBLog = {
+      'title': 'reddit',
+      'author': 'redditors',
+      'url': 'https://reddit.com',
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBLog)
+    const newBlogs = await helper.blogsInDb()
+    console.log(newBlogs)
+    expect(newBlogs[newBlogs.length - 1].likes).toBe(0)
+  })
 
-test('blogs without likes property will have it set to 0', async () => {
-  const newBLog = {
-    'title': 'reddit',
-    'author': 'redditors',
-    'url': 'https://reddit.com',
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBLog)
-  const newBlogs = await helper.blogsInDb()
-  console.log(newBlogs)
-  expect(newBlogs[newBlogs.length - 1].likes).toBe(0)
-})
-
-test('creating a blog without a title or url returns error 400 bad request', async () => {
-  const newBlogNoAuthor = {
-    'author': 'redditors',
-    'url': 'https://reddit.com',
-    'likes': 3
-  }
-  const newBlogNoUrl = {
-    'title': 'reddit',
-    'author': 'redditors',
-    'likes': 3
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlogNoAuthor)
-    .expect(400)
-  await api
-    .post('/api/blogs')
-    .send(newBlogNoUrl)
-    .expect(400)
+  test('creating a blog without a title or url returns error 400 bad request', async () => {
+    const newBlogNoAuthor = {
+      'author': 'redditors',
+      'url': 'https://reddit.com',
+      'likes': 3
+    }
+    const newBlogNoUrl = {
+      'title': 'reddit',
+      'author': 'redditors',
+      'likes': 3
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlogNoAuthor)
+      .expect(400)
+    await api
+      .post('/api/blogs')
+      .send(newBlogNoUrl)
+      .expect(400)
+  })
 })
 
 afterAll(() => {
