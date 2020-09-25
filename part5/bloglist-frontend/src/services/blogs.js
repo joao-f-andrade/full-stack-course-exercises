e4d57e2f05ba5bdcda9586b447dc2f4d2c84bfb4
awoken-coder <jfafandrade@gmail.com>
2020-09-25 12:42:00 +0100

    getAll service now sorts blogs by number of likes in descending order

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -2,9 +2,11 @@ import axios from 'axios'
 const baseUrl = '/api/blogs'
 
 let token = null
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const getAll = async () => {
+  const request = await axios.get(baseUrl)
+  request.data.sort((a,b)=>b.likes-a.likes)
+  console.log('getBlogs', request.data)
+  return request.data
 }
 
 const setToken = newToken => {
