ecbc33bde066376069024751edc329c4a0a97078
awoken-coder <jfafandrade@gmail.com>
2020-09-25 12:28:49 +0100

    function now sends id of user in user property instead of totally deleting user property

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -10,22 +10,23 @@ const getAll = () => {
 const setToken = newToken => {
   return token = `bearer ${newToken}`
 }
-const saveBlog = async (token,body) => {
-  console.log('token e body',token,body)
+const saveBlog = async (token, body) => {
+  console.log('token e body', token, body)
   const config = {
-        headers: { Authorization: token },
-        }
+    headers: { Authorization: token },
+  }
   const request = await axios.post(baseUrl,
     body,
-  config
+    config
   )
-  return(request.data)
+  return (request.data)
 }
-const likeBlog = async (token,body) => {
+const likeBlog = async (token, body) => {
   console.log('token e body', token, body)
   body.likes += 1
-  delete body.user
-  const request = await axios.put(`${baseUrl}/${body.id}`,body)
-  return(request)
+  console.log('body.user', body.user)
+  body.user = body.user.id
+  const request = await axios.put(`${baseUrl}/${body.id}`, body)
+  return (request)
 }
 export default { getAll, setToken, saveBlog, likeBlog }
