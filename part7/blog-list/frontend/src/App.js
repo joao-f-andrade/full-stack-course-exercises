823d9d0744fbe59c67aff7becb2a25c93a2b846b
awoken-coder <jfafandrade@gmail.com>
2020-11-10 17:17:18 +0200

    changed log in login to inside the loginform component

--- part7/blog-list/frontend/src/App.js
+++ part7/blog-list/frontend/src/App.js
@@ -1,19 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import BlogList from './components/BlogList'
 import blogService from './services/blogs'
-import loginService from './services/login'
 import Notification from './components/Notification'
 import LoginForm from './components/LoginForm'
 import NewBlogForm from './components/NewBlogForm'
 import Togglable from './components/Togglable'
 import { useDispatch, useSelector } from 'react-redux'
-import { setNotification } from './reducers/notificationReducer'
-import { addBlog, initializeBlogs } from './reducers/blogReducer'
+import { initializeBlogs } from './reducers/blogReducer'
 import { addUser } from './reducers/userReducer'
 
 const App = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const user = useSelector(state => state.user)
 
   const dispatch = useDispatch()
@@ -33,42 +29,17 @@ const App = () => {
       dispatch(addUser(newUser))
     }
   }, [])
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    try {
-      const user = await loginService.login({
-        username, password
-      })
-      dispatch(addUser(user))
-      console.log('newUser', user)
-      setUsername('')
-      setPassword('')
-      window.localStorage.setItem(
-        'loggedUser', JSON.stringify(user)
-      )
-      user.token = blogService.setToken(user.token)
-    } catch (exception) {
-      dispatch(setNotification('Wrong credentials', 5))
-    }
-  }
 
   const logOut = () => {
     dispatch(addUser(null))
     window.localStorage.removeItem('loggedUser')
   }
-  const loginForm = () => (
-    <LoginForm
-      handleSubmit={handleLogin}
-      handlePassChange={({ target }) => setPassword(target.value)}
-      handleUsernameChange={({ target }) => setUsername(target.value)}
-    />
-  )
 
   return (
     <div>
       <Notification />
       {user === null ?
-        loginForm() :
+        <LoginForm /> :
         <div>
           <h2>blogs</h2>
           <p> {user.name} <button type='button' onClick={logOut} >Log out</button> </p>
--- part7/blog-list/frontend/src/components/LoginForm.js
+++ part7/blog-list/frontend/src/components/LoginForm.js
@@ -1,10 +1,36 @@
-import React from 'react'
-const LoginForm = ({
-  handleUsernameChange,
-  handlePassChange,
-  handleSubmit
-}) => (
+import React, {useState} from 'react'
+import loginService from '../services/login'
+import { setNotification } from '../reducers/notificationReducer'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../reducers/userReducer'
+import blogService from '../services/blogs'
 
+const LoginForm = () => {
+  const dispatch = useDispatch()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      dispatch(addUser(user))
+      console.log('newUser', user)
+      setUsername('')
+      setPassword('')
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
+      user.token = blogService.setToken(user.token)
+    } catch (exception) {
+      dispatch(setNotification('Wrong credentials', 5))
+    }
+  }
+  const handlePassChange=({ target }) => setPassword(target.value)
+  const handleUsernameChange=({ target }) => setUsername(target.value)
+
+return (
   <form onSubmit={handleSubmit}>
     <h2>Log In</h2>
     <div>
@@ -27,6 +53,6 @@ const LoginForm = ({
     </div>
     <button type="submit" id='login-button'>login</button>
   </form>
-)
+)}
 
 export default LoginForm
