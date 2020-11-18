c51649155463f35e0a8198279516688f05bfa116
awoken-coder <jfafandrade@gmail.com>
2020-11-18 17:24:46 +0200

    added reducer for searching all authors

--- part8/index.js
+++ part8/index.js
@@ -88,7 +88,7 @@ const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: Author!
+    author: String!
     id: ID!
     genres: [String!]
   }
@@ -100,13 +100,17 @@ const typeDefs = gql`
   type Query {
     bookCount: Int
     authorCount: Int
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books,
+    allAuthors: () => authors
   }
 }
 
